76c035643af9f7504b03c7bc7b7b63cd1f22b35c
jrnalts <jrnalts@gmail.com>
2020-10-28 20:35:14 +0800

    fix error

--- app/controllers/nekocat_controller.rb
+++ app/controllers/nekocat_controller.rb
@@ -7,7 +7,7 @@ class NekocatController < ApplicationController
     # 學說話
     reply_text = learn(received_text)
 
-    # 設定回覆文字
+    # 關鍵字回覆
     reply_text = keyword_reply(received_text) if reply_text.nil?
 
     # 傳送訊息到 Line
@@ -23,18 +23,36 @@ class NekocatController < ApplicationController
     message['text'] if not message.nil?
   end
 
+  # 學說話
+  def learn(received_text)
+    # 如果開頭不是 內扣貓你說； 就跳出
+    return nil if not received_text[0..5] == '內扣貓你說；'
+
+    received_text = received_text[6..-1]
+    semicolon_index = received_text.index('；')
+
+    # 找不到分號就跳出
+    return nil if semicolon_index.nil?
+
+    keyword = received_text[ 0..semicolon_index - 1 ]
+    message = received_text[ semicolon_index + 1..-1 ]
+
+    KeywordMapping.create(keyword: keyword, message: message)
+    '好~喵嗚~'
+  end
+
   # 關鍵字回覆
   def keyword_reply(received_text)
     # 如果 DB裡有關鍵字，就將查詢結果存到 mapping
     KeywordMapping.where(keyword: received_text).last&.message
 
-    # 學習紀錄表
-    keyword_mapping = {
-      '好聽的歌' => '夜に駆ける：https://www.youtube.com/watch?v=x8VYWazR5mE'
-    }
+    # # 學習紀錄表
+    # keyword_mapping = {
+    #   '好聽的歌' => '夜に駆ける：https://www.youtube.com/watch?v=x8VYWazR5mE'
+    # }
 
     # 查表
-    keyword_mapping[received_text]
+    # keyword_mapping[received_text]
   end
 
   # 傳送訊息到 Line
@@ -54,23 +72,6 @@ class NekocatController < ApplicationController
     line.reply_message(reply_token, message)
   end
 
-  # 學說話
-  def learn(received_text)
-    # 如果開頭不是 內扣貓你說; 就跳出
-    return nil if not received_text[0..5] == '內扣貓你說;'
-
-    received_text = received_text[6..-1]
-    semicolon_index = received_text.index(';')
-
-    # 找不到分號就跳出
-    return nil if semicolon_index.nil?
-
-    keyword = received_text[0..semicolon_index-1]
-    message = received_text[semicolon_index+1..-1]
-
-    KeywordMapping.create(keyword: keyword, message: message)
-    '好~喵嗚~'
-  end
 
   # Line Bot API object initial
   def line
