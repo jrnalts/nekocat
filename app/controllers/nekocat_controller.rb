2dec5ca1dcdac1b87f0aa2d6ad912a8dfc46ae91
jrnalts <jrnalts@gmail.com>
2020-10-28 19:28:03 +0800

    串接 Line channel

--- app/controllers/nekocat_controller.rb
+++ app/controllers/nekocat_controller.rb
@@ -1,3 +1,5 @@
+require 'line/bot'
+
 class NekocatController < ApplicationController
   protect_from_forgery with: :null_session
 
@@ -30,7 +32,26 @@ class NekocatController < ApplicationController
   end
 
   def webhook
-    render plain: params
+    # Line Bot API initial
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = 'dbf120339521248cd77ba07fcfec1541'
+      config.channel_token = 'pAUzHQHj9q5Rd+JYysvToRrhV/QGYxMqB4fWEM0JsnNkPfvdkohFyzXzjXy9Pk9aHMpoRCPgxRIb6fRYK0Dm2xEnLnpLQ1lmKgJGKgHJXDc01DgKRDgxIFAGODp/XZ/vcn3BcQOAJXNz6JPk4dOaUQdB04t89/1O/w1cDnyilFU='
+    }
+
+    # gey reply token
+    reply_token = params['events'][0]['replyToken']
+
+    # set reply message
+    message = {
+      type: 'text'
+      text: '好～喵嗚～'
+    }
+
+    # send message
+    response = client.reply_message(reply_token, message)
+
+    # respond 200
+    head :ok
   end
 
   def sent_request
