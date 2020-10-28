8d604adc65880a07d094cc386d8d30d187d4dc39
jrnalts <jrnalts@gmail.com>
2020-10-28 19:34:33 +0800

    fix bug/nekocat_controller

--- app/controllers/nekocat_controller.rb
+++ app/controllers/nekocat_controller.rb
@@ -3,34 +3,6 @@ require 'line/bot'
 class NekocatController < ApplicationController
   protect_from_forgery with: :null_session
 
-  def index
-    render plain: "罐罐呢？"
-  end
-
-  def request_headers
-    render plain: request.headers.to_h.reject{ |key, value| 
-      key.include? '.'
-    }.map { |key, value|
-      "#{key}: #{value}"
-    }.sort.join("\n")
-  end
-
-  def request_body
-    render plain: request.body
-  end
-
-  def response_headers
-    render plain: response.headers.to_h.map { |key, value|
-      "#{key}: #{value}"
-    }.sort.join("\n")
-  end
-
-  def show_response_body
-    puts "===這是設定後的response.body:#{response.body}==="
-    render plain: "歐拉歐拉歐拉"
-    puts "===這是設定後的response.body:#{response.body}==="
-  end
-
   def webhook
     # Line Bot API initial
     client = Line::Bot::Client.new { |config|
@@ -43,7 +15,7 @@ class NekocatController < ApplicationController
 
     # set reply message
     message = {
-      type: 'text'
+      type: 'text',
       text: '好～喵嗚～'
     }
 
@@ -54,21 +26,49 @@ class NekocatController < ApplicationController
     head :ok
   end
 
-  def sent_request
-    uri = URI('http://localhost:3000/nekocat/eat')
-    http = Net::HTTP.new(uri.host, uri.port)
-    http_request = Net::HTTP::Get.new(uri)
-    http_response = http.request(http_request)
+  # def index
+  #   render plain: "罐罐呢？"
+  # end
 
-    render plain: JSON.pretty_generate({
-      request_class: request.class,
-      response_class: response.class,
-      http_request_class: http_request.class,
-      http_response_class: http_response.class
-    })
-  end
+  # def request_headers
+  #   render plain: request.headers.to_h.reject{ |key, value| 
+  #     key.include? '.'
+  #   }.map { |key, value|
+  #     "#{key}: #{value}"
+  #   }.sort.join("\n")
+  # end
 
-  def translate_to_korean(message)
-    "#{message}油~"
-  end
+  # def request_body
+  #   render plain: request.body
+  # end
+
+  # def response_headers
+  #   render plain: response.headers.to_h.map { |key, value|
+  #     "#{key}: #{value}"
+  #   }.sort.join("\n")
+  # end
+
+  # def show_response_body
+  #   puts "===這是設定後的response.body:#{response.body}==="
+  #   render plain: "你正在看response body!"
+  #   puts "===這是設定後的response.body:#{response.body}==="
+  # end
+
+  # def sent_request
+  #   uri = URI('http://localhost:3000/nekocat/eat')
+  #   http = Net::HTTP.new(uri.host, uri.port)
+  #   http_request = Net::HTTP::Get.new(uri)
+  #   http_response = http.request(http_request)
+
+  #   render plain: JSON.pretty_generate({
+  #     request_class: request.class,
+  #     response_class: response.class,
+  #     http_request_class: http_request.class,
+  #     http_response_class: http_response.class
+  #   })
+  # end
+
+  # def translate_to_korean(message)
+  #   "#{message}油~"
+  # end
 end
