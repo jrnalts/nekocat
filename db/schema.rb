dd2edeb029d5fbaa4ffb063bf4e98cffa63383ba
jrnalts <jrnalts@gmail.com>
2020-10-29 00:32:46 +0800

    建立model received, model reply

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_084801) do
+ActiveRecord::Schema.define(version: 2020_10_28_163135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,20 @@ ActiveRecord::Schema.define(version: 2020_10_28_084801) do
   create_table "keyword_mappings", force: :cascade do |t|
     t.string "keyword"
     t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "receiveds", force: :cascade do |t|
+    t.string "channel_id"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.string "channel_id"
+    t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
