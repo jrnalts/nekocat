d16eb47195b9cc81c121f066fecb2608e55d0504
jrnalts <jrnalts@gmail.com>
2020-10-28 17:25:52 +0800

    測試 request & response

--- app/controllers/nekocat_controller.rb
+++ app/controllers/nekocat_controller.rb
@@ -1,5 +1,33 @@
-class NekocatController < application_controller
+class NekocatController < ApplicationController
   def index
     render plain: "罐罐呢？"
+  end
+
+  def request_headers
+    render plain: request.headers.to_h.reject{ |key, value| 
+      key.include? '.'
+    }.map { |key, value|
+      "#{key}: #{value}"
+    }.sort.join("\n")
+  end
+
+  def request_body
+    render plain: request.body
+  end
+
+  def response_headers
+    render plain: response.headers.to_h.map { |key, value|
+      "#{key}: #{value}"
+    }.sort.join("\n")
+  end
+
+  def show_response_body
+    puts "===這是設定後的response.body:#{response.body}==="
+    render plain: "虎挖花哈哈哈"
+    puts "===這是設定後的response.body:#{response.body}==="
+  end
+
+  def webhook
+    head :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/nekocat/index', to: 'nekocat#index'
+  get '/nekocat/request_headers', to: 'nekocat#request_headers'
+  get '/nekocat/request_body', to: 'nekocat#request_body'
+
+  get '/nekocat/response_headers', to: 'nekocat#response_headers'
+  get '/nekocat/response_body', to: 'nekocat#show_response_body'
+
+  post '/nekocat/webhook', to: 'nekocat#webhook'
 end
