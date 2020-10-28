e481744ba077fcdcdfee0f8318366f6da7f88050
jrnalts <jrnalts@gmail.com>
2020-10-29 00:44:02 +0800

    完成推齊功能

--- app/controllers/nekocat_controller.rb
+++ app/controllers/nekocat_controller.rb
@@ -79,8 +79,38 @@ class NekocatController < ApplicationController
     line.reply_message(reply_token, message)
   end
 
+  # 頻道 ID
+  def channel_id
+    source = params['events'][0]['source']
+    source['groupId'] || source['roomId'] || source['userId']
+  end
+  
+  # 儲存對話
+  def save_to_received(channel_id, received_text)
+    return if received_text.nil?
+    Received.create(channel_id: channel_id, text: received_text)
+  end
 
-  # Line Bot API object initial
+  # 儲存回應
+  def save_to_reply
+    return if reply_text.nil?
+    Reply.create(channel_id: channel_id, text: reply_text)
+  end
+  
+  # 推齊功能
+  def echo2(channel_id, received_text)
+    # 如果在 channel_id 最近沒人講過 received_text，內扣貓就不回應
+    recent_received_texts = Received.where(channel_id: channel_id).last(5)&.pluck(:text)
+    return nil if not received_text.in? recent_received_texts
+
+    # 如果在 channel_id 內扣貓上一句回應是 received_text，內扣貓就不回應
+    last_reply_text = Reply.where(channel_id: channel_id).last&.text
+    return nil if last_reply_text == received_text
+
+    received_text
+  end
+
+  # Line Bot API initialize
   def line
     @line ||= Line::Bot::Client.new { |config|
       config.channel_secret = 'dbf120339521248cd77ba07fcfec1541'
